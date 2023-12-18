ff969e35fcd5c58584372cca194a0e73b6d5f9e3
abrahamnjuguna <abrahamnjuguna@outlook.com>
2023-12-18 08:45:25 +0300

    added database port

--- blog/settings.py
+++ blog/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': '',
+        'PORT': '5432',
     }
     
 }
